d58980f1137acaad2a54aa3eaf3de19f67eec108
SabirLume <36938996+SabirLume@users.noreply.github.com>
2019-02-24 17:48:04 -0500

    Add files via upload

---
+++ slot-machine-2019a-week05/README.md
@@ -0,0 +1,17 @@
+# My Awesome Project
+For this project I made a slot machine. There are two buttons for a minimum and max bet. The user can either win or lose money.
+Write a short sentence or two about this project and what it does. Be sure to include a link and a screenshot (we're front end devs so we can actually see our work!).
+
+
+
+![alt text](slot.png)
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript, Framework of choice
+I made this web application with both JavaScript, HTML, and CSS.The user starts with $1,000. When a minimum bet is made, the user can lose up to one dollar or make 25 dollars. When a maximum bet is made the user can make up to 100 dollars or lose 50 dollars. When the user loses the dom is updated with the message of "winner" or "loser".
+
+
+## Lessons Learned:
+
+While making this web application I further my understanding of how arguments work within a function. Also my understanding of conditionals has increased.
---
+++ slot-machine-2019a-week05/js/main.js
@@ -0,0 +1,93 @@
+// Build a simple slot machine with minimum 5
+// items per reel and 3 reels - user should be
+// able to bet min or max and have their total update
+var total = 1000;
+
+//when we click the min button it called the
+//randomReel function
+document.getElementById('clickMin').addEventListener("click", function(){
+  var reel1 = randomReel()
+  var reel2 = randomReel()
+  var reel3 = randomReel()
+  console.log(reel1,reel2,reel3)
+  // conditional to upadate total values
+
+  var results = checkMatch(reel1, reel2, reel3)
+  if(results==='winner!'){
+    total = total + 25;
+  }else{
+    total = total - 1;
+
+  }
+
+  //update the dom with new total
+  document.getElementById('finalResult').innerHTML = total
+  document.getElementById('response').innerHTML = results
+  document.getElementById('reel1').innerHTML = reel1
+  document.getElementById('reel2').innerHTML = reel2
+  document.getElementById('reel3').innerHTML = reel3
+});
+
+document.getElementById('clickMax').addEventListener("click", function(){
+  var reel1 = randomReel()
+  var reel2 = randomReel()
+  var reel3 = randomReel()
+  console.log(reel1,reel2,reel3)
+
+  // conditional to upadate total values
+
+  var results = checkMatch(reel1, reel2, reel3)
+  if(results==='winner!'){
+    total = total + 100;
+  }else{
+    total = total - 50;
+
+  }
+
+  //update the dom with new total
+  document.getElementById('finalResult').innerHTML = total
+  document.getElementById('response').innerHTML = results
+  document.getElementById('reel1').innerHTML = reel1
+  document.getElementById('reel2').innerHTML = reel2
+  document.getElementById('reel3').innerHTML = reel3
+});
+
+document.getElementById('clickMax').addEventListener("click", function(){
+
+});
+
+//choose random value for the reel
+
+function randomReel(){
+
+  var choice = Math.random()
+  if (choice <= .2){
+    return 'cherry';
+  }else if(choice <=.4){
+    return 'mango';
+  }else if(choice <=.6){
+    return 'carrot';
+  }else if(choice <=.8){
+    return 'orange';
+  }else{
+    return 'money';
+
+  }
+  return choice;
+
+//1 ut of 5
+// return the value
+
+}
+
+//compare reels to find if there are a winner
+function checkMatch(a, b, c){
+  if (a == b && b == c){
+    return 'winner!';
+  }else{
+    return 'loser!'
+  }
+
+}
+
+//compare reels find
